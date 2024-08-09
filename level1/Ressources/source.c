8aa8dbaa018851a0ceab503c002986eecfb955ba
Hassine Radhoin <rahassin@student.42.fr>
2024-08-10 00:36:37 +0200

    add ressources level1

--- level1/Ressources/source.c
+++ level1/Ressources/source.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void run() {
-    char *data = (char *)0x80497c0;
-    const char *filename = (const char *)0x8048570;
-    int count = 19; // 0x13 + 1 (for 0-based index)
-    int size = 1;
-
-    fwrite(data, size, count, fopen(filename, "w"));
-    system((const char *)0x8048584);
+int run(void) 
+{
+	fwrite("Good... Wait what?\n", 1, 19, stdout);
+	return system("/bin/sh");
 }
 
-int main() {
-    char buffer[80]; // Allocate space for input
-    printf("Enter input: ");
-    gets(buffer, sizeof(buffer), stdin);
-}
+int main(int argc, char **argv)
+{
+	char str[80];
 
+	gets(str);
+	return (0);
+}
